595ed3c23bc8727542cf513499572540953200d5
andrewboles <andrewboles@me.com>
2022-02-16 12:08:27 -0600

    re-init

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     // static: {
     //   directory: path.join(__dirname, 'src'),
     // },
-    static: ['./src'],
+    static: ['./dist'],
     compress: true,
     port: 9000,
   },
