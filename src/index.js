9628bbfaab5e38330dbc10ef7807c41281ca1434
Andrew Boles <andrewboles@me.com>
2022-05-23 09:17:37 -0700

    changed copy

--- src/index.js
+++ src/index.js
@@ -20,76 +20,76 @@ animate();
 
 function init() {
 
-    container = document.createElement( 'div' );
+    container = document.createElement('div');
     let hero = document.getElementById('hero')
-    hero.appendChild( container );
-  ;
+    hero.appendChild(container);
+    ;
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 1000;
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x363945 );
+    scene.background = new THREE.Color(0x363945);
 
     //
 
     const numParticles = AMOUNTX * AMOUNTY;
 
-    const positions = new Float32Array( numParticles * 3 );
-    const scales = new Float32Array( numParticles );
+    const positions = new Float32Array(numParticles * 3);
+    const scales = new Float32Array(numParticles);
 
     let i = 0, j = 0;
 
-    for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+    for (let ix = 0; ix < AMOUNTX; ix++) {
 
-        for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+        for (let iy = 0; iy < AMOUNTY; iy++) {
 
-            positions[ i ] = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 ); // x
-            positions[ i + 1 ] = 0; // y
-            positions[ i + 2 ] = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 ); // z
+            positions[i] = ix * SEPARATION - ((AMOUNTX * SEPARATION) / 2); // x
+            positions[i + 1] = 0; // y
+            positions[i + 2] = iy * SEPARATION - ((AMOUNTY * SEPARATION) / 2); // z
 
-            scales[ j ] = 1;
+            scales[j] = 1;
 
             i += 3;
-            j ++;
+            j++;
 
         }
 
     }
 
     const geometry = new THREE.BufferGeometry();
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
-    geometry.setAttribute( 'scale', new THREE.BufferAttribute( scales, 1 ) );
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    geometry.setAttribute('scale', new THREE.BufferAttribute(scales, 1));
 
-    const material = new THREE.ShaderMaterial( {
+    const material = new THREE.ShaderMaterial({
 
         uniforms: {
-            color: { value: new THREE.Color( 0x89908c ) },
+            color: { value: new THREE.Color(0x89908c) },
         },
-        vertexShader: document.getElementById( 'vertexshader' ).textContent,
-        fragmentShader: document.getElementById( 'fragmentshader' ).textContent
+        vertexShader: document.getElementById('vertexshader').textContent,
+        fragmentShader: document.getElementById('fragmentshader').textContent
 
-    } );
-
-    //
-
-    particles = new THREE.Points( geometry, material );
-    scene.add( particles );
+    });
 
     //
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
+    particles = new THREE.Points(geometry, material);
+    scene.add(particles);
+
+    //
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
 
 
     container.style.touchAction = 'none';
-    container.addEventListener( 'pointermove', onPointerMove );
+    container.addEventListener('pointermove', onPointerMove);
 
     //
 
-    window.addEventListener( 'resize', onWindowResize );
+    window.addEventListener('resize', onWindowResize);
 
 }
 
@@ -101,15 +101,15 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 
 //
 
-function onPointerMove( event ) {
+function onPointerMove(event) {
 
-    if ( event.isPrimary === false ) return;
+    if (event.isPrimary === false) return;
 
     mouseX = event.clientX - windowHalfX;
     mouseY = event.clientY - windowHalfY;
@@ -120,7 +120,7 @@ function onPointerMove( event ) {
 
 function animate() {
 
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
 
     render();
 
@@ -128,27 +128,27 @@ function animate() {
 
 function render() {
 
-    camera.position.x += ( mouseX - camera.position.x ) * .05;
-    camera.position.y += ( - mouseY - camera.position.y ) * .05;
-    camera.lookAt( scene.position );
+    camera.position.x += (mouseX - camera.position.x) * .05;
+    camera.position.y += (- mouseY - camera.position.y) * .05;
+    camera.lookAt(scene.position);
 
     const positions = particles.geometry.attributes.position.array;
     const scales = particles.geometry.attributes.scale.array;
 
     let i = 0, j = 0;
 
-    for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+    for (let ix = 0; ix < AMOUNTX; ix++) {
 
-        for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+        for (let iy = 0; iy < AMOUNTY; iy++) {
 
-            positions[ i + 1 ] = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
-                            ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+            positions[i + 1] = (Math.sin((ix + count) * 0.3) * 50) +
+                (Math.sin((iy + count) * 0.5) * 50);
 
-            scales[ j ] = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 20 +
-                            ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 20;
+            scales[j] = (Math.sin((ix + count) * 0.3) + 1) * 20 +
+                (Math.sin((iy + count) * 0.5) + 1) * 20;
 
             i += 3;
-            j ++;
+            j++;
 
         }
 
@@ -157,7 +157,7 @@ function render() {
     particles.geometry.attributes.position.needsUpdate = true;
     particles.geometry.attributes.scale.needsUpdate = true;
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 
     count += 0.1;
 
